f8ea23be490d6f0cce5c8b2690781729ffc81126
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-13 21:48:30 +0530

    Jul 13  21:48:30

--- 16-trianglearea-Python/trianglearea.py
+++ 16-trianglearea-Python/trianglearea.py
@@ -4,6 +4,12 @@
 # will probably wish to use Heron's Formula.
 
 
+def islegaltriangle(s1, s2, s3):
+    return (s1 > 0 and s2 > 0 and s3 > 0) and (max(s1, s2, s3) < s1 + s2 + s3 - max(s1, s2, s3))
+
+
 def trianglearea(s1, s2, s3):
-	# your code goes here
-	pass
+    # your code goes here
+    if not islegaltriangle(s1, s2, s3):
+        return 0
+    return (0.25) * ((s1 + s2 + s3) * (s1 + s2 - s3) * (s3 + s1 - s2) * (s2 + s3 - s1))**0.5
