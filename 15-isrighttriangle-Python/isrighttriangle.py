85356a77c67fd58fc02f1c32da1edf85db690fdd
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-13 21:22:26 +0530

    Jul 13  21:22:26

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -8,7 +8,7 @@ import math
 
 
 def distance(x1, y1, x2, y2):
-    return sqrt((x2-x1)**2 + (y2-y1)**2)
+    return math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
