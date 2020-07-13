64d7edf865b984eaa8db231eae070b3abaacd7a9
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-13 21:26:19 +0530

    Jul 13  21:26:19

--- 03-distance-Python/distance.py
+++ 03-distance-Python/distance.py
@@ -3,6 +3,7 @@
 # that represent the two points (x1, y1) and (x2, y2),
 # and returns the distance between those points as a int.
 
+
 def fun_distance(x1, y1, x2, y2):
     # your code goes here
     return int(((x2 - x1)**2 + (y2 - y1)**2)**(0.5))
