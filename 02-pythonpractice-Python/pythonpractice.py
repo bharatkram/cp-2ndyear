4a11082be0070079347a16be6bd344784027b15d
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-13 17:09:03 +0530

    Jul 13  17:09:03

--- 02-pythonpractice-Python/pythonpractice.py
+++ 02-pythonpractice-Python/pythonpractice.py
@@ -15,6 +15,20 @@ with them:
 Everything else has 0 points.
 Use the test cases below to guide you!"""
 
+
 class Classy(object):
     def __init__(self):
         self.items = []
+
+    def Class(item, self):
+        self.items.append(item)
+
+    def classiness(self):
+        score = 0
+        itemsDict = {"tophat": 2, "bowtie": 4, "monocle": 5}
+        for item in self.items:
+            try:
+                score += itemsDict[item]
+            except:
+                continue
+        return score
