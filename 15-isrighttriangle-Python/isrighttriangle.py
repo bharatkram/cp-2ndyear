57589702e44d094fd03faad0c51bcbe39523e495
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-13 21:40:38 +0530

    Jul 13  21:40:38

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -13,6 +13,6 @@ def distance(x1, y1, x2, y2):
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
     # your code goes here
-    first, second, third = [distance(x1, y1, x2, y2), distance(
-        x2, y2, x3, y3), distance(x3, y3, x1, y1)].sort()
+    first, second, third = sorted([distance(x1, y1, x2, y2), distance(
+        x2, y2, x3, y3), distance(x3, y3, x1, y1)])
     return math.isclose(third**2, first**2 + second**2)
