21e35c746a4755d87c84fab7dc4f46e611490058
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-13 18:09:49 +0530

    Jul 13  18:09:49

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -2,6 +2,7 @@
 # a non-negative integer number of eggs, and returns
 # the smallest integer number of cartons required to hold
 # that many eggs, where a carton may hold up to 12 eggs
+import math
 
 
 def fun_eggcartons(eggs):
