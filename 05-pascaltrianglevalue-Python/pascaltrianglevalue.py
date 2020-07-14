0391c32a7cd6e0f9f7a1a37fff473dc6ef64340b
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-14 20:35:47 +0530

    Jul 14  20:35:47

--- 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
+++ 05-pascaltrianglevalue-Python/pascaltrianglevalue.py
@@ -4,10 +4,15 @@
 # Triangle where the triangle starts at row 0, and
 # each row starts at column 0. If either row or col
 # are not legal values, return None, instead of crashing.
+import math
+
+
+def coeff(n, k):
+    return math.factorial(n) / (math.factorial(k) * math.factorial(n - k)) if n != 0 else 0
 
 
 def fun_pascaltrianglevalue(row, col):
     # your code goes here
     if row < 0 or col < 0 or row + 1 < col:
         return 0
-    return row**col
+    return coeff(row - 1, col - 1) + coeff(row - 1, col)
