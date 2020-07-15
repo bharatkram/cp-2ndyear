dbaae5375b9d96ca15e070b06c975656266a7aff
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-15 15:05:39 +0530

    Jul 15  15:05:39

--- 04-threelines_area-Python/threelines_area.py
+++ 04-threelines_area-Python/threelines_area.py
@@ -1,9 +1,8 @@
-# Write the function fun_threelines_area(d1, d2, d3) 
+# Write the function fun_threelines_area(d1, d2, d3)
 # that takes length of 3 sides
 # find the area of a triangle(return an int) given its side lengths.
-
 import math
 
+
 def fun_threelines_area(a, b, c):
-	return 0
-	
+    return 0.25 * ((s1 + s2 + s3) * (s1 + s2 - s3) * (s2 + s3 - s1) * (s3 + s1 - s2))**0.5
