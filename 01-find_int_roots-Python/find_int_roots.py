f95604ad3f3c9e10c6dd092ac84dc8cf7678b792
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-15 14:31:41 +0530

    Jul 15  14:31:41

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -9,4 +9,5 @@ import math
 
 
 def fun_find_int_roots(a, b, c):
-    return sorted([round((-b / (2*a)) + (b**2 - 4*a*c)**0.5), round((-b / (2*a) - (b**2 - 4*a*c)**0.5))])
+    d = round((b**2 - 4*a*c)**0.5)
+    return sorted([round(-b / (2*a) + d), round(-b / (2*a) - d)])
