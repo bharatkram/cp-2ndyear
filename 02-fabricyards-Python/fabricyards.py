dc8817f02fd2fb6da8393ec7c2aa76452c9f3627
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-15 14:54:05 +0530

    Jul 15  14:54:05

--- 02-fabricyards-Python/fabricyards.py
+++ 02-fabricyards-Python/fabricyards.py
@@ -1,23 +1,23 @@
 # fabricyards(inches)
-# Fabric must be purchased in whole yards, so purchasing just 1 inch 
-# of fabric requires purchasing 1 entire yard. With this in mind, 
-# write the function fabricYards(inches) that takes the number of 
-# inches of fabric desired, and returns the smallest number of whole 
+# Fabric must be purchased in whole yards, so purchasing just 1 inch
+# of fabric requires purchasing 1 entire yard. With this in mind,
+# write the function fabricYards(inches) that takes the number of
+# inches of fabric desired, and returns the smallest number of whole
 # yards of fabric that must be purchased.
 
 # fabricexcess(inches)
-# Write the function fabricExcess(inches) that takes the number of 
-# inches of fabric desired and returns the number of inches of excess 
+# Write the function fabricExcess(inches) that takes the number of
+# inches of fabric desired and returns the number of inches of excess
 # fabric that must be purchased (as purchases must be in whole yards)
 # . Hint: you may want to use fabricYards, which you just wrote!
+from math import ceil
 
 
 def fun_fabricyards(inches):
-	# your code goes here
-	return 1
+    # your code goes here
+    return ceil(inches/36)
+
 
 def fun_fabricexcess(inches):
-	# your code goes here
-	return 1
-
-
+    # your code goes here
+    return fun_fabricyards(inches) * 36 - inches
