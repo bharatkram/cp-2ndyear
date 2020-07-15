c3d25eb70b07ab1a357a609b1f54551997636571
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-15 14:14:04 +0530

    Jul 15  14:14:04

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -1,13 +1,12 @@
-# Write the function bonusFindIntRoots(a,b,c) that takes 
+# Write the function bonusFindIntRoots(a,b,c) that takes
 # the int coefficients a, b, c of a quadratic equation of this form:
 #      y = ax2 + bx + c
-# You are guaranteed the function has 2 real roots, and in fact that 
-# the roots are all integers. Your function should return these 2 int roots 
+# You are guaranteed the function has 2 real roots, and in fact that
+# the roots are all integers. Your function should return these 2 int roots
 # in increasing order. How does a function return multiple values? Like so:
 # return root1, root2
-
 import math
+
+
 def fun_find_int_roots(a, b, c):
-	return 0, 0
-
-
+    return (-b / (2*a))*(b**2 - 4*a*c)*0.5
