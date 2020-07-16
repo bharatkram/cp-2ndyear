5ced69f53963f2720c398d2514b6de39ea741987
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-16 16:55:37 +0530

    Jul 16  16:55:37

--- 08-isrotated-Python/isrotated.py
+++ 08-isrotated-Python/isrotated.py
@@ -4,7 +4,8 @@
 # "XYZ" and "YXZ" then return false.
 
 
-def checkfunction(str1, str2, index):
+def checkfunction(str1, tup):
+    str2, index = tup
     str2 = str2[index:] + str2[:index]
     return str1 == str2
 
@@ -15,15 +16,15 @@ def isrotated(str1, str2):
         return False
     indexes = []
     char = str1[0]
-    for letter, index in enumerate(str2):
+    for index, letter in enumerate(str2):
         if letter == char:
             indexes.append(index)
     for index in indexes:
         check = str1[1]
         direction = 1 if check == str2[index +
-                                       1] else -1 if check == str2[index - 1] else 0
+                                       1 if index != len(str2) - 1 else 0] else -1 if check == str2[index - 1 if index != 0 else len(str2) - 1] else 0
         ret = checkfunction(str1, (str2, index) if direction ==
-                            1 else (str2[::-1], len(str2)-index)) if direction != 0 else False
+                            1 else (str2[::-1], len(str2)-(index+1))) if direction != 0 else False
         if ret == True:
             return True
     return False
