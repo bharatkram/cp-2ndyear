81b3d508bcca9c846e5bb94250113fbcae7cad8a
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-18 12:01:15 +0530

    Jul 18  12:01:15

--- 08-multipolynomials-Python/multipolynomials.py
+++ 08-multipolynomials-Python/multipolynomials.py
@@ -1,10 +1,19 @@
-# Background: we can represent a polynomial as a list of its coefficients. For example, [2, 3, 0, 4] could represent 
-# the polynomial 2x3 + 3x2 + 4. With this in mind, write the function multiplyPolynomials(p1, p2) which takes two 
-# lists representing polynomials as just described, and returns a third list representing the polynomial which is 
-# the product of the two. For example, multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x + 
+# Background: we can represent a polynomial as a list of its coefficients. For example, [2, 3, 0, 4] could represent
+# the polynomial 2x3 + 3x2 + 4. With this in mind, write the function multiplyPolynomials(p1, p2) which takes two
+# lists representing polynomials as just described, and returns a third list representing the polynomial which is
+# the product of the two. For example, multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x +
 # 5), and:    (2x**2 + 3)(4x + 5) = 8x**3 + 10x**2 + 12x + 15
 # And so this returns the list [8, 10, 12, 15].
 
+
 def multipolynomials(p1, p2):
-	# Your code goes here
-	pass
+    # Your code goes here
+    ret = [0] * (len(p1) + len(p2) - 1)
+    l1 = len(p1)
+    l2 = len(p2)
+    for i in range(l1):
+        exp1 = l1 - (i + 1)
+        for j in range(l2):
+            exp2 = l2 - (j + 1)
+            ret[exp1 + exp2 - 1] += p1[i]*p2[j]
+    return ret
