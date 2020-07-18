f5feac88784b5bd58c4ad431b040a5b01c653a12
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-18 20:47:40 +0530

    Jul 18  20:47:39

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -1,6 +1,26 @@
 """Implement quick sort in Python.
 Input a list.
 Output a sorted list."""
+
+
+def partition(array, low, high):
+    i = low - 1
+    pivot = array[high]
+    for j in range(low, high):
+        if array[j] < pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+    return i+1
+
+
+def qsort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        qsort(array, low, pi + 1)
+        qsort(array, pi + 1, high)
+
+
 def quicksort(array):
-	# Your code goes here
-	pass
+    # Your code goes here
+    qsort(array, 0, len(array) - 1)
