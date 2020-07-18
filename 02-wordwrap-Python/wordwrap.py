8c24c2afcfa073b3cf31b2e6c63b8644f90a9acb
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-18 10:36:29 +0530

    Jul 18  10:36:29

--- 02-wordwrap-Python/wordwrap.py
+++ 02-wordwrap-Python/wordwrap.py
@@ -1,9 +1,9 @@
 # Write the function wordWrap(text, width) that takes a string of text (containing only lowercase letters
-#  or spaces) and a positive integer width, and returns a possibly-multiline string that matches the 
-# original string, only with line wrapping at the given width. So wordWrap("abc", 3) just returns "abc", 
-# but wordWrap("abc",2) returns a 2-line string, with "ab" on the first line and "c" on the second line. 
-# After you complete word wrapping in this way, only then: All spaces at the start and end of each 
-# resulting line should be removed, and then all remaining spaces should be converted to dashes ("-"), 
+#  or spaces) and a positive integer width, and returns a possibly-multiline string that matches the
+# original string, only with line wrapping at the given width. So wordWrap("abc", 3) just returns "abc",
+# but wordWrap("abc",2) returns a 2-line string, with "ab" on the first line and "c" on the second line.
+# After you complete word wrapping in this way, only then: All spaces at the start and end of each
+# resulting line should be removed, and then all remaining spaces should be converted to dashes ("-"),
 # so they can be easily seen in the resulting string. Here are some test cases for you:
 # assert(wordWrap("  abcdefghij", 4)  ==  """\
 # abcd
@@ -17,7 +17,11 @@
 
 
 def fun_wordwrap(s, n):
-	return ""
-
-
- 
+    pos = 0
+    ret, s = "", s.trim()
+    while pos <= len(s):
+        char = s[pos]
+        ret += char if char != " " else "-"
+        if pos % n == 0 and pos != 0:
+            ret += "\n"
+    return ret
