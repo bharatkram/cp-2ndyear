54193f11885bb6131ab9fd1a5d21962fb1125610
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-22 15:55:12 +0530

    Jul 22  15:55:12

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -17,7 +17,7 @@ def longestdigitrun(n):
         n //= 10
         if dig != tempDigit:
             if tempCount > count:
-                count = tempCount + 1
+                count = tempCount
                 digit = tempDigit
             elif tempCount == count:
                 digit = tempDigit if tempDigit < digit else digit
