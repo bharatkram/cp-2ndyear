59d8f1e7af00a70b6ef1f88307fb859ef27fd9c6
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-22 14:45:50 +0530

    Jul 22  14:45:50

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -1,12 +1,41 @@
-# Write the function nthSmithNumber that takes a non-negative int n 
-# and returns the nth Smith number, where a Smith number is a composite (non-prime) 
-# the sum of whose digits are the sum of the digits of its prime factors (excluding 1). 
-# Note that if a prime number divides the Smith number multiple times, its digit sum 
-# is counted that many times. For example, 4 equals 2**2, so the prime factor 2 is 
+# Write the function nthSmithNumber that takes a non-negative int n
+# and returns the nth Smith number, where a Smith number is a composite (non-prime)
+# the sum of whose digits are the sum of the digits of its prime factors (excluding 1).
+# Note that if a prime number divides the Smith number multiple times, its digit sum
+# is counted that many times. For example, 4 equals 2**2, so the prime factor 2 is
 # counted twice, thus making 4 a Smith Number.
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
+from math import sqrt
+
+
+def sumOfDigits(n):
+    total = 0
+    while n != 0:
+        total += n % 10
+        n //= 10
+    return total
+
+
+def primeFactors(n):
+    lis = []
+    for i in range(2, int(sqrt(n))+1):
+        while n % i == 0:
+            n //= i
+            lis.append(i)
+    return lis
+
+
+def isSmithNumber(n):
+    lisOfFactors = primeFactors(n)
+    return sumOfDigits(n) == sumOfDigits(sum(lisOfFactors))
 
 
 def fun_nth_smithnumber(n):
-    return 1
+    # your code goes here
+    num = 3
+    while n != -1:
+        num += 1
+        if isSmithNumber(n):
+            n -= 1
+    return num
