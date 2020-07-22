83a1158700323daafa83f444887d9bc89e64ddfa
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-22 16:13:11 +0530

    Jul 22  16:13:11

--- 05-nthwithproperty309-Python/test_nthwithproperty309.py
+++ 05-nthwithproperty309-Python/test_nthwithproperty309.py
@@ -1,20 +1,20 @@
-import os,sys
-sys.path.append(os.getcwd())
-from nthwithproperty309 import nthwithproperty309
 import pytest
+from nthwithproperty309 import nthwithproperty309
+import os
+import sys
+sys.path.append(os.getcwd())
 
 
-@pytest.mark.parametrize('x, result',[
-	(0, 309),
-	(418, 1),
-	(462, 2),
-	(474, 3),
-	(575, 4),
-	(635, 5),
-	(662, 6),
-	(2014, 100),
-	(7813, 1000)
+@pytest.mark.parametrize('result, x', [
+    (309, 0),
+    (418, 1),
+    (462, 2),
+    (474, 3),
+    (575, 4),
+    (635, 5),
+    (662, 6),
+    (2014, 100),
+    (7813, 1000)
 ])
-
 def test_nthwithproperty309(x, result):
     assert nthwithproperty309(x) == result
