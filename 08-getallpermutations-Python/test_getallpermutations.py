2c8f828694269fec0fa62015707ceb7180610a06
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-26 20:22:35 +0530

    Jul 26  20:22:35

--- 08-getallpermutations-Python/test_getallpermutations.py
+++ 08-getallpermutations-Python/test_getallpermutations.py
@@ -1,14 +1,15 @@
-import os,sys
-sys.path.append(os.getcwd())
-from getallpermutations import getallpermutations
-import pytest
 from itertools import permutations
+import pytest
+from getallpermutations import getallpermutations
+import os
+import sys
+sys.path.append(os.getcwd())
 
-@pytest.mark.parametrize('x',[
-	("abc"), ("abcd"), ("ab"), ("abcde"), 
-	("pqr"), ("pqrs"), ("pq"), ("pqrst"), 
-	("xyz"), ("xyza"), ("xy"), ("xyzab"),
+
+@pytest.mark.parametrize('x', [
+    ("abc"), ("abcd"), ("ab"), ("abcde"),
+    ("pqr"), ("pqrs"), ("pq"), ("pqrst"),
+    ("xyz"), ("xyza"), ("xy"), ("xyzab"),
 ])
-
 def test_getallpermutations(x):
-    assert getallpermutations(x) == list(permutations(x, r=len(x)))
+    assert getallpermutations(x) == sorted(list(permutations(x, r=len(x))))
