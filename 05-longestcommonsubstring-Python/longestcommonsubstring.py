ff528e1dddcd9c859d5c32358e1af5746d7d2296
bharat ram koppu <bharat.bharatkram@msitprogram.net>
2020-07-26 19:24:47 +0530

    Jul 26  19:24:47

--- 05-longestcommonsubstring-Python/longestcommonsubstring.py
+++ 05-longestcommonsubstring-Python/longestcommonsubstring.py
@@ -7,17 +7,27 @@
 # compare the strings). So, for example:
 # longestCommonSubstring("abcABC", "zzabZZAB") returns "AB" and not "ab"
 
+
 def longestcommonsubstring(s1, s2):
     # Yourcode goes here
-    ret = ""
-    len1, len2 = len(s1), len(s2)
-    for i in range(len1):
-        match = ""
-        for j in range(len2):
-            if (i + j < len1) and (s1[i+j] == s2[j]):
-                match += s2[j]
-        else:
-            if len(match) > len(ret):
-                ret = match
-            match = ""
-    return ret
+    l1, l2 = len(s1), len(s2)
+    l, pos = 0, -1
+
+    c = [[-1]*(l2+1) for i in range(l1 + 1)]
+
+    for i in range(l1 + 1):
+        c[i][l2] = 0
+    for j in range(l2):
+        c[l1][j] = 0
+
+    for i in range(l1 - 1, -1, -1):
+        for j in range(l2):
+            if s1[i] != s2[j]:
+                c[i][j] = 0
+            else:
+                c[i][j] = 1 + c[i+1][j+1]
+                if l < c[i][j]:
+                    l = c[i][j]
+                    pos = i
+
+    return s1[pos: pos + l] if l > 0 else ""
